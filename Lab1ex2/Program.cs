7d9f7320c94783e3c251e5319a6cc7f0f19c3612
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-06-27 13:01:23 +0300

    Add project files.

---
+++ Lab1ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab1ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ex2
+            Scrieti un program care va calcula media aritmetica a trei numere citite de la
+            tastatura*/
+
+            Console.WriteLine("Calculeaza media aritmetica");
+
+            Console.WriteLine("Introduceti primul numar: ");
+            int primulNumar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar: ");
+            int alDoileaNumar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al trilea numar: ");
+            int alTreileaNumar = int.Parse(Console.ReadLine());
+
+            int mediaAritmetica = (primulNumar + alDoileaNumar + alTreileaNumar) / 3;
+
+            Console.WriteLine("Media aritmetica este: " + mediaAritmetica);
+
+
+        }
+    }
+}
